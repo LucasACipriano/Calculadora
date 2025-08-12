cfc1b41c30f1bb101897ef8dd9daf2bd8e894b87
LucasACipriano <lucasalvaradocipriano@gmail.com>
2025-08-11 22:37:01 -0300

    Update README.py

--- README.py
+++ README.py
@@ -1,8 +1,20 @@
 #importando tkinter
 from tkinter import *
-from tkinter import tkk 
+from tkinter import ttk 
 
-janela = tk()
-jenela.title("calculadora")
+#cores
+preta = "#21130d"
+
+janela=Tk()
+janela.title("Calculadora")
+janela.geometry("235x318")
+janela.config(bg=preta)
+
+Frame_tela = Frame(janela, width=235, height=50)
+Frame_tela.grid(row=0, column=0,)
+
+Frame_corpo = Frame(janela, width=235, height=268)
+Frame_corpo.grid(row=1, column=0)
+
 
 janela.mainloop()
